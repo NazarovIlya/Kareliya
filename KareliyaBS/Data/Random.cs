3ffaf3b4a48db0387fe3d91d16ba91520f2c72fc
NazarovIlya <eliakohai@gmail.com>
2022-12-03 23:07:08 +0300

    Blazer Server Kateliya project initial

---
+++ KareliyaBS/Data/Random.cs
@@ -0,0 +1,12 @@
+﻿namespace KareliyaBS.Data
+{
+	public class Randomize
+	{
+		public static int Page()
+		{
+			Random rnd = new Random();
+			int random = rnd.Next(0, 3);
+			return random;
+		}
+	}
+}
